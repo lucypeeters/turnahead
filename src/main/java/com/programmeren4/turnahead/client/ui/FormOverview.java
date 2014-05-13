4725fbe1e2a0eae1f4da362b3b23120040c2a75a
lucypeeters <lucypeeters02@gmail.com>
2014-05-13 22:25:07 +0200

    views +gameview

--- src/main/java/com/programmeren4/turnahead/client/ui/FormOverview.java
+++ src/main/java/com/programmeren4/turnahead/client/ui/FormOverview.java
@@ -2,13 +2,11 @@ package com.programmeren4.turnahead.client.ui;
 
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
-import com.google.gwt.event.logical.shared.SelectionEvent;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.uibinder.client.UiHandler;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Composite;
-import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.VerticalPanel;
 import com.google.gwt.user.client.ui.Widget;
 
--- src/main/java/com/programmeren4/turnahead/client/ui/Home.java
+++ src/main/java/com/programmeren4/turnahead/client/ui/Home.java
@@ -2,6 +2,9 @@ package com.programmeren4.turnahead.client.ui;
 
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.event.logical.shared.ValueChangeEvent;
+import com.google.gwt.event.logical.shared.ValueChangeHandler;
+import com.google.gwt.user.client.History;
 
 public class Home implements EntryPoint {
 
@@ -11,7 +14,35 @@ public class Home implements EntryPoint {
 	@Override
 	public void onModuleLoad() {
 		RootPanel.get().add(application);
-	}
+		
+		/*History.addValueChangeHandler(new ValueChangeHandler<String>() {
+			@Override
+			public void onValueChange(ValueChangeEvent<String> event) {
+				if (){
+					
+					greetingService.getLocationByID(event.getValue().split("/")[1], new AsyncCallback<String>() {
+
+					@Override
+					public void onSuccess(String result) {
+					
+					}
+
+					@Override
+					public void onFailure(Throwable caught) {
+					// TODO Auto-generated method stub
+
+					}
+					});
+					}
+
+					event.getValue();
+					}
+					});*/
 
 
+			}
+	
+
+
+	
 }
